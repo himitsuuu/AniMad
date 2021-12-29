9334ebb227646034f3252c16c9497481595ef221
himitsuuu <92147743+himitsuuu@users.noreply.github.com>
2021-12-29 21:58:04 +0800

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,8 +10,8 @@ const Footer = () => {
                         <div class="col-md-12">
                             <div class="copyright-text">
                                 <p>
-                                    <a href="https://github.com/NamanBalaji/AniMad">Repo</a> Created by 
-                                    <a href="https://www.linkedin.com/in/naman-balaji-a35602178/" className='link'> Naman Balaji</a>
+                                    <a href="">Site</a> Created by 
+                                    <a href="" className='link'> Kimiko</a>
                                 </p>
                             </div>
                         </div>
